9849634fde7be329db4d7ea32781d9a7d081e687
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2016-03-07 14:20:10 +0800

    Initial commit

---
+++ generate_yaml_file.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export page_size=5000
+
+for cs in $( echo "1 10 100 1K" ); do
+	for vs in $( echo "50 500 5K 500K 5MB" ); do
+		export value_size=$vs
+		export clustering_size=$cs
+		export table_name=value${value_size}_cluster${clustering_size}
+		echo "Generating ${table_name}"
+		
+		envsubst < bulk_read_template.yaml > bulk_read_${table_name}.yaml
+	done
+done
+
+
