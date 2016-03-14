4b269887c64d62f25af461ef9d80870434b68b42
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2016-03-14 10:57:37 +0800

    Modified page sizes so as to keep each op at 1MB

--- generate_yaml_file.sh
+++ generate_yaml_file.sh
@@ -2,14 +2,18 @@
 
 export page_size=5000
 
+# the value sizes are chosen so that two columns with this size give the size described in the table name
 value_sizes=("50" "512" "5120" "51200" "524288")
 table_names=("100_bytes" "1_kbyte" "10_kbytes" "100_kbytes" "1_mbyte")
+# the page size is chosen so that each operation (a page size download) corresponds to 1MB
+page_sizes=("10000" "1000" "100" "10" "1")
 
 for i in `seq 0 4`
 do
 	export clustering_size="1"
 	export value_size=${value_sizes[$i]}
 	export table_name="data_${table_names[$i]}"
+	export page_size=${page_sizes[$i]}
 
 	echo "Generating ${table_name}"
 	envsubst < bulk_read_template.yaml > bulk_read_${table_names[$i]}.yaml
@@ -24,6 +28,7 @@ do
 	export value_size=${value_sizes[$i]}
 	name="100_kbytes_with_${clustering_size}_rows"
 	export table_name="data_${name}"
+	export page_size="10"
 
 	echo "Generating ${table_name}"
 	envsubst < bulk_read_template.yaml > bulk_read_${name}.yaml
--- measure.sh
+++ measure.sh
@@ -4,11 +4,12 @@ set -xv #echo on
 rm bulk-read-benchmark.html
 
 yaml_files=("100_bytes" "1_kbyte" "10_kbytes" "100_kbytes" "1_mbyte")
-sizes=("1M" "1M" "100K" "10K" "1K")
+sizes=("1M" "1M" "1M" "250K" "25K")
 title=bulk_read_by_partition_size
+threads=("50" "50" "10" "1" "1")
 
-#yaml_files=("100_kbytes" "100_kbytes_with_100_rows" "100_kbytes_with_1000_rows")
-#title=bulk_read_by_clustering_size
+#conf/cassandra.yaml:
+#write_request_timeout_in_ms: 30000
 
 prefix="https://raw.githubusercontent.com/stef1927/cstar_bulk_read_test/master/"
 
@@ -16,8 +17,8 @@ for i in `seq 0 4`;
 do
     yaml_file="${prefix}bulk_read_${yaml_files[$i]}.yaml"
     echo "Processing ${yaml_file}"
-    ./tools/bin/cassandra-stress user profile=${yaml_file} ops\(insert=1\) n=${sizes[$i]} -rate threads=50
-    ./tools/bin/cassandra-stress user profile=${yaml_file} ops\(all_columns_tr_query=1\) n=5K -rate threads=25 -tokenrange wrap -graph file=bulk-read-benchmark.html title=${title} revision=${yaml_files[$i]}
-    cqlsh -e "drop keyspace bulk_read"
+    #./tools/bin/cassandra-stress user profile=${yaml_file} ops\(insert=1\) n=${sizes[$i]} no-warmup -rate threads=${threads[$i]}
+    ./tools/bin/cassandra-stress user profile=${yaml_file} ops\(all_columns_tr_query=1\) n=30K -rate threads=1 -tokenrange wrap -graph file=bulk-read-benchmark.html title=${title} revision=${yaml_files[$i]} op=bulk_read
+    #cqlsh -e "drop keyspace bulk_read"
 done
 
