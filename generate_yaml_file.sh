6020738ff5858cca94d4e2b879bba9456a0fc6d2
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2016-03-14 09:20:07 +0800

    Changed file naming and reduced multi-clustering tests

--- generate_yaml_file.sh
+++ generate_yaml_file.sh
@@ -2,15 +2,31 @@
 
 export page_size=5000
 
-for cs in $( echo "1 10 100 1k" ); do
-	for vs in $( echo "50 500 5k 50k 500k 5m" ); do
-		export value_size=$vs
-		export clustering_size=$cs
-		export table_name=value${value_size}_cluster${clustering_size}
-		echo "Generating ${table_name}"
-		
-		envsubst < bulk_read_template.yaml > bulk_read_${table_name}.yaml
-	done
+value_sizes=("50" "512" "5120" "51200" "524288")
+table_names=("100_bytes" "1_kbyte" "10_kbytes" "100_kbytes" "1_mbyte")
+
+for i in `seq 0 4`
+do
+	export clustering_size="1"
+	export value_size=${value_sizes[$i]}
+	export table_name="data_${table_names[$i]}"
+
+	echo "Generating ${table_name}"
+	envsubst < bulk_read_template.yaml > bulk_read_${table_names[$i]}.yaml
+done
+
+
+value_sizes=("512" "50")
+clustering_sizes=("100" "1000")
+for i in `seq 0 1`
+do
+	export clustering_size=${clustering_sizes[$i]}
+	export value_size=${value_sizes[$i]}
+	name="100_kbytes_with_${clustering_size}_rows"
+	export table_name="data_${name}"
+
+	echo "Generating ${table_name}"
+	envsubst < bulk_read_template.yaml > bulk_read_${name}.yaml
 done
 
 
